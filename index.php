a5b56ffd44c0d6e68d43a638484c5c43a95743ac
shinehope <sana_tsm@yahoo.com>
2020-07-04 16:21:06 +0100

    Update index.php

--- index.php
+++ index.php
@@ -72,6 +72,6 @@ if(array_key_exists('city',$_GET)){
     <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
     <script type="text/javascript" src="jqueryfile.js"></script>
-    <script type="text/javascript" src="https://maps.googleapis.com/maps/api/js?key=AIzaSyDP8lNXMd15_KbqbMuQ_x6SoPhFbhhBjWI&libraries=places&callback=activatePlacesSearch"></script>    
+    <script type="text/javascript" src="></script>    
 </body>
 </html>
